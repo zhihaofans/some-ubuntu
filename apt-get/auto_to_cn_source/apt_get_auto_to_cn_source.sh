86ab4df35c59f6f20243d12b58fa0a83378695fe
zhihaofans <zhihaofans@hotmail.com>
2017-06-08 10:57:18 +0800

    Update "/apt-get/auto_to_cn_source/apt_get_auto_to_cn_source.sh"

--- apt-get/auto_to_cn_source/apt_get_auto_to_cn_source.sh
+++ apt-get/auto_to_cn_source/apt_get_auto_to_cn_source.sh
@@ -10,24 +10,25 @@ echo "3:16.04"
 echo "4:14.04"
 read ver
 echo $ver
-if [ $ver -eq 1 ]
-then
-    echo "You choose 17.04"
-    downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/17.04/sources.list"
-elif [ $ver -eq 2 ]
-then
-    echo "You choose 16.10"
-    downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/16.10/sources.list"
-elif [ $ver -eq 3 ]
-then
-    echo "You choose 16.04"
-    downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/16.04/sources.list"
-elif [ $ver -eq 4 ]
-then
-    echo "You choose 14.04"
-    downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/14.04/sources.list"
+if [ $ver ]; then
+    if [ $ver -eq 1 ];then
+        echo "You choose 17.04"
+        downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/17.04/sources.list"
+    elif [ $ver -eq 2 ];then
+        echo "You choose 16.10"
+        downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/16.10/sources.list"
+    elif [ $ver -eq 3 ];then
+        echo "You choose 16.04"
+        downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/16.04/sources.list"
+    elif [ $ver -eq 4 ];then
+        echo "You choose 14.04"
+        downUrl="https://github.com/zhihaofans/some-ubuntu/raw/master/apt-get/auto_to_cn_source/14.04/sources.list"
+    else
+        echo "Error OS version,end"
+        exit 1
+    fi
 else
-    echo "Error OS version,end"
+    echo "Error OS version,end."
     exit 1
 fi
 wget -P /etc/apt/ $downUrl
